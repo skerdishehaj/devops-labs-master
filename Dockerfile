4407aecd8d31e4c38b0239d76833aa65a0dcd662
Skerdi Shehaj <skerdianshehaj@gmail.com>
2024-01-16 22:17:00 +0100

    Updated to add logs and volumes in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM eclipse-temurin:19-alpine
-VOLUME /tmp
+
+RUN mkdir -p /var/logs
+VOLUME /var/logs
 EXPOSE 8080
+
 WORKDIR /backend
 ARG JAR_FILE=./target/spring-boot-rest-api-postgresql-0.0.1-SNAPSHOT.jar
 ADD ${JAR_FILE} fti.jar
+
 ENTRYPOINT ["java","-jar","fti.jar"]
--- src/main/java/com/hendisantika/springbootrestapipostgresql/controller/BookRestController.java
+++ src/main/java/com/hendisantika/springbootrestapipostgresql/controller/BookRestController.java
@@ -2,6 +2,9 @@ package com.hendisantika.springbootrestapipostgresql.controller;
 
 import com.hendisantika.springbootrestapipostgresql.entity.Book;
 import com.hendisantika.springbootrestapipostgresql.repository.BookRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,33 +34,43 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/books")
 public class BookRestController {
+    private static final Logger logger = LoggerFactory.getLogger(BookRestController.class);
 
     @Autowired
     private BookRepository repository;
 
     @PostMapping
     public ResponseEntity<?> addBook(@RequestBody Book book) {
+        logger.debug("addBook({}) called", book.getName());
+        logger.info("Adding book {}", book);
         return new ResponseEntity<>(repository.save(book), HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<Collection<Book>> getAllBooks() {
+        logger.debug("getAllBooks() called");
+        logger.info("Getting all books");
         return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Book> getBookWithId(@PathVariable Long id) {
+        logger.debug("getBookWithId() called with: id = [" + id + "]");
+        logger.info("Getting book with id {}", id);
         return new ResponseEntity<Book>(repository.findById(id).get(), HttpStatus.OK);
     }
 
-    @GetMapping(params = {"name"})
+    @GetMapping(params = { "name" })
     public ResponseEntity<Collection<Book>> findBookWithName(@RequestParam(value = "name") String name) {
+        logger.debug("findBookWithName() called with: name = [" + name + "]");
+        logger.info("Getting book with name {}", name);
         return new ResponseEntity<>(repository.findByName(name), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Book> updateBookFromDB(@PathVariable("id") long id, @RequestBody Book book) {
-
+        logger.debug("updateBookFromDB() called with: id = [" + id + "], book = [" + book + "]");
+        logger.info("Updating book with id {}", id);
         Optional<Book> currentBookOpt = repository.findById(id);
         Book currentBook = currentBookOpt.get();
         currentBook.setName(book.getName());
@@ -69,11 +82,15 @@ public class BookRestController {
 
     @DeleteMapping("/{id}")
     public void deleteBookWithId(@PathVariable Long id) {
+        logger.debug("deleteBookWithId() called with: id = [" + id + "]");
+        logger.info("Deleting book with id {}", id);
         repository.deleteById(id);
     }
 
     @DeleteMapping
     public void deleteAllBooks() {
+        logger.debug("deleteAllBooks() called");
+        logger.info("Deleting all books");
         repository.deleteAll();
     }
 }
